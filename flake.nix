e4d00c682fc676ec7861af27fe665721b33d8962
George Shammas <george@shamm.as>
2023-05-18 07:19:48 -0400

    more work

--- flake.nix
+++ flake.nix
@@ -1,13 +1,56 @@
 {
   inputs = {
-    opam-nix.url = "github:tweag/opam-nix";
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.follows = "opam-nix/nixpkgs";
+    systems.url = "github:nix-systems/x86_64-linux";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    opam-nix = {
+      url = "github:tweag/opam-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+    ocaml-flambda = { url = "github:ocaml-flambda/flambda-backend/4.14.1-11"; flake = false; };
+    opam-default = { url = "github:ocaml/opam-repository"; flake = false; };
+    opam-beta = { url = "github:ocaml/ocaml-beta-repository"; flake = false; };
+    opam-jane = { url = "github:janestreet/opam-repository"; flake = false; };
+    opam-jane-external = { url = "github:janestreet/opam-repository/external-packages"; flake = false; };
+    opam-dune-universe = { url = "github:dune-universe/opam-overlays"; flake = false; };
   };
-  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
+  outputs = { self, flake-utils, opam-nix, nixpkgs, ... }@inputs:
+    let
+      main-overlay = final: prev: {
+        ocaml = final.ocamlPackages.ocaml;
+        ocamlPackages_old = prev.ocamlPackages;
+        ocamlPackages = prev.ocamlPackages.overrideScope' (ofinal: oprev: {
+          ocaml = prev.stdenv.mkDerivation
+            {
+              name = "ocaml";
+              version = "4.14.1-jst11";
+              src = inputs.ocaml-flambda;
+              strictDeps = true;
+              nativeBuildInputs = with prev; [ which rsync autoreconfHook automake final.ocamlPackages_old.ocaml final.ocamlPackages_old.dune_3 ];
+              buildInputs = with prev; [ ncurses ];
+              propagatedBuildInputs = with prev; [ libunwind ];
+              prefixKey = "-prefix ";
+              configureFlags = [ "--enable-middle-end=closure" "--enable-legacy-library-layout" ];
+              postBuild = ''
+                mkdir -p $out/include
+                ln -sv $out/lib/ocaml/caml $out/include/caml
+              '';
+              buildTargets = ["compiler" ];
+              installTargets = [ "install"  ];
+              passthru = {
+                nativeCompilers = true;
+              };
+              meta = oprev.ocaml.meta;
+            };
+        });
+      };
+    in
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; overlays = [ main-overlay ]; }; # nixpkgs.legacyPackages.${system};
         on = opam-nix.lib.${system};
         localPackagesQuery = builtins.mapAttrs (_: pkgs.lib.last)
           (on.listRepo (on.makeOpamRepo ./.));
@@ -19,16 +62,29 @@
         query = devPackagesQuery // {
           ## You can force versions of certain packages here, e.g:
           ## - force the ocaml compiler to be taken from opam-repository:
-          ocaml-base-compiler = "*";
+          # ocaml-base-compiler = "*";
+          ocaml-base-compiler = "4.14.0";
           ## - or force the compiler to be taken from nixpkgs and be a certain version:
-          # ocaml-system = "4.14.0";
+          # ocaml-system = "4.14.1";
           ## - or force ocamlfind to be a certain version:
           # ocamlfind = "1.9.2";
+          # ocaml-flambda = "*";
         };
-        scope = on.buildOpamProject' { } ./. query;
+        scope = on.buildOpamProject'
+          {
+            repos = with inputs; [
+              opam-default
+              ./opam-repository
+              opam-beta
+              # opam-jane
+              # opam-jane-external
+              # opam-dune-universe
+            ];
+          } ./.
+          query;
         overlay = final: prev:
           {
-            # You can add overrides here
+            ocaml-base-compiler = prev.ocaml-base-compiler.overrideAttrs (oa: { nativeBuildInputs = oa.nativeBuildInputs ++ (with pkgs; [ autoconf libtool which rsync automake dune_3 ocaml ]); });
           };
         scope' = scope.overrideScope' overlay;
         # Packages from devPackagesQuery
@@ -37,11 +93,18 @@
         # Packages in this workspace
         packages =
           pkgs.lib.getAttrs (builtins.attrNames localPackagesQuery) scope';
-      in {
+      in
+      {
         legacyPackages = scope';
 
-        inherit packages;
-
+        packages = packages // {
+          ocaml = pkgs.ocaml;
+          ocamlPackages = pkgs.ocamlPackages;
+        };
+        overlay = [
+          (final: prev:
+            { })
+        ];
         ## If you want to have a "default" package which will be built with just `nix build`, do this instead of `inherit packages;`:
         # packages = packages // { default = packages.<your default package>; };
 
